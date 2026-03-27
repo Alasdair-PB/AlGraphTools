c4236cbed9ec7fbbac31a4dcf419423b8652a697
GeckoGamez <39963762+Alasdair-PB@users.noreply.github.com>
2026-03-27 21:49:37 +0000

    Next steps thoughts

--- Assets/DialogueGraph.cs
+++ Assets/DialogueGraph.cs
@@ -6,15 +6,20 @@ using UnityEngine;
 using System.Linq;
 
 
-public class DialogueGraph : GTGraph {
+// Brainstroming starting node- >
+// Shouldn't use attributes as this is a runtime method
+// templated classes could work, but may stop it from being serialized
 
-    public SequenceAct GetStartingAct() => startingNode.GetConnectedAct();
 
-    [field: SerializeField] private GTPerformanceConnection startingNode;
+public class DialogueGraph : GTGraph<SequenceAct> {
+
+    public SequenceAct GetStartingAct() => startisNode.GetConnectedAct();
+
+    [field: SerializeField] private GTPerformanceConnection startisNode;
 
     private void SaveStartingNode(Dictionary<GTNodeData, GTNodeData> graphToObjectMap)
     {
-        foreach (var (getter, setter) in startingNode.GetAllReferences())
+        foreach (var (getter, setter) in startisNode.GetAllReferences())
         {
             var oldRef = getter();
             if (oldRef != null && graphToObjectMap.ContainsKey(oldRef))
@@ -26,7 +31,7 @@ public class DialogueGraph : GTGraph {
     {
         var reverseMap = graphToObjectMap.ToDictionary(kv => kv.Value, kv => kv.Key);
 
-        foreach (var (getter, setter) in startingNode.GetAllReferences())
+        foreach (var (getter, setter) in startisNode.GetAllReferences())
         {
             var currentValue = getter();
             if (currentValue != null && reverseMap.TryGetValue(currentValue, out var key))
--- Packages/com.alasdair.graphtools/Runtime/Scripts/Data/GTGraph.cs
+++ Packages/com.alasdair.graphtools/Runtime/Scripts/Data/GTGraph.cs
@@ -3,16 +3,24 @@ using UnityEngine;
 
 namespace GT.Data.Save
 {
-    public class GTGraph : ScriptableObject
+    // Use attributes to describe ports?
+
+
+    public abstract class GTGraph<StartingNode> : ScriptableObject where StartingNode : GTNodeData
     {
-        [field: SerializeField] public string FileName { get; set; }
+        [field: SerializeField] public string FileName { get; set; } // Move to graph view
         [field: SerializeField] public List<GTGroupSaveData> Groups { get; set; }
-        [field: SerializeReference] public List<GTNodeData> Nodes { get; set; }
+        [field: SerializeReference] public List<GTNodeData> Nodes { get; set; } 
+        [field:SerializeReference] public StartingNode startingNode { get; set; }
         [field: SerializeField] public List<string> OldGroupNames { get; set; }
         [field: SerializeField] public List<string> OldUngroupedNodeNames { get; set; }
         [field: SerializeField] public SerializableDictionary<string, List<string>> OldGroupedNodeNames { get; set; }
 
+        // Make this abstract and don't keep nodes here?
+        // Could go thorugh property fields and get all node data
         public virtual List<GTNodeData> GetNodes() => Nodes;
+
+        // Public create Property window -> expose graph variables to edit in the graph view.
 
         public GTGraph()
         {
